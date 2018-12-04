dc314377c9b1e3efdb44d2c9779887b21f43bc06
Soporte Ceiba <soporteceiba@MacBook-Pro-de-Usuario.local>
2018-12-04 09:37:34 -0500

    Initial commit

---
+++ src/main/java/com/ceiba/parking/AdnParkingApplication.java
@@ -0,0 +1,12 @@
+package com.ceiba.parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdnParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdnParkingApplication.class, args);
+	}
+}
